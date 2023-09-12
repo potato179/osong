f04fab28207f2eb313a1d597edaa3793e862e5f6
홍은표 <44293278+potato179@users.noreply.github.com>
2023-09-12 10:27:05 +0900

    pretty osong

--- package/index.js
+++ package/index.js
@@ -1,5 +1,5 @@
 function osong(){
-    console.log("  ─┓    \n      ┃    \n        \\    \n         \\    \n          X    \n         /   \\    \n        /      \\    \n       /         \\    \n ┏┛          \\    \n ┗");
+    console.log("  ─┓    \n   ┃    \n    \\    \n     \\    \n      X    \n     / \\    \n    /   \\    \n   /     \\    \n ┏┛       \\    \n ┗");
 }
 
 module.exports = osong;
